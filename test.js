8a2e2965b0906ba5a8b72fa7c143f1d95a26e8eb
GreenMan <133495573+ChelovekNeWerea25@users.noreply.github.com>
2024-06-19 18:21:59 +0300

    Create test.js

---
+++ test.js
@@ -0,0 +1,26 @@
+function checkURLInList(currentURL, listURL) {
+  // Загрузите список URL-адресов с другого сайта
+  fetch(listURL)
+    .then(response => response.text())
+    .then(data => {
+      // Преобразуйте список в массив
+      const urlList = data.split('\n');
+
+      // Проверьте, есть ли текущий URL в списке
+      if (urlList.includes(currentURL)) {
+        console.log('Текущий URL (' + currentURL + ') находится в списке.');
+      } else {
+        console.log('Текущий URL (' + currentURL + ') не найден в списке.');
+      }
+    })
+    .catch(error => console.error('Ошибка при загрузке списка URL:', error));
+}
+
+// Получите текущий URL-адрес
+const currentURL = window.location.href;
+
+window.onload = function() {
+  const listURL = 'https://example.com/list.txt'; // Замените 'example.com/list.txt' на URL вашего списка
+  checkURLInList(currentURL, listURL);
+  document.write('<h1>Стиль не одобрен на этом сайте!</h1><h2>Купить/изменить лицензию для сайта, или изменить сайт<a href="https://t.me/GreenShopCrmp_bot"> тут</a> или <a href="https://t.me/werea25">тут</a></h2><a href="/admin.php">Админ панель для отключения тут</a>');
+};
