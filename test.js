30e785db72ed597aea7cf60a3cac4ff46b0d6965
GreenMan <133495573+ChelovekNeWerea25@users.noreply.github.com>
2024-06-19 18:23:07 +0300

    Update test.js

--- test.js
+++ test.js
@@ -20,7 +20,7 @@ function checkURLInList(currentURL, listURL) {
 const currentURL = window.location.href;
 
 window.onload = function() {
-  const listURL = 'https://example.com/list.txt'; // Замените 'example.com/list.txt' на URL вашего списка
+  const listURL = 'https://raw.githubusercontent.com/ChelovekNeWerea25/licens/main/lic.txt'; // Замените 'example.com/list.txt' на URL вашего списка
   checkURLInList(currentURL, listURL);
   document.write('<h1>Стиль не одобрен на этом сайте!</h1><h2>Купить/изменить лицензию для сайта, или изменить сайт<a href="https://t.me/GreenShopCrmp_bot"> тут</a> или <a href="https://t.me/werea25">тут</a></h2><a href="/admin.php">Админ панель для отключения тут</a>');
 };
