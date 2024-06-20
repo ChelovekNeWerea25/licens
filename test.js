ae84dc50332c92dd7a8b0fc2fd9b5f00bc647d9f
GreenMan <133495573+ChelovekNeWerea25@users.noreply.github.com>
2024-06-20 10:34:39 +0300

    Update test.js

--- test.js
+++ test.js
@@ -10,7 +10,7 @@ function checkURLInList(currentURL, listURL) {
       if (urlList.includes(currentURL)) {
         console.log('Текущий URL (' + currentURL + ') находится в списке.');
       } else {
-        document.write('<h1>Стиль не одобрен на этом сайте!</h1><h2>Купить/изменить лицензию для сайта, или изменить сайт <a href="https://t.me/GreenShopCrmp_bot">тут</a> или <a href="https://t.me/werea25">тут</a></h2><a href="/admin.php">Админ панель для отключения тут</a>');
+        document.write('<h1>Лицензия не найдена!</h1><h2>Купить/изменить лицензию для сайта, или изменить сайт <a href="https://t.me/GreenShopCrmp_bot">тут</a> или <a href="https://t.me/werea25">тут</a></h2><a href="/admin.php">Админ панель для отключения тут</a>');
       }
     })
     .catch(error => console.error('Ошибка при загрузке списка URL:', error));
